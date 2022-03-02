ab906ba72b8af369ef9cee6a3caae1e9fa8e0b67
Alyssa Cuson <acuson98@gmail.com>
2022-03-02 17:39:38 -0500

    removed routes folder, added server,js

---
+++ server.js
@@ -0,0 +1,9 @@
+// Require express
+const express = require('express');
+
+// Import modular routers
+const notesRouter = require('./notes');
+
+const app = express();
+app.use('/notes', notesRouter);
+module.exports = app;
