63436b41ed5f996a9bf08256e63f948721ef8528
Alyssa Cuson <acuson98@gmail.com>
2022-03-02 19:49:25 -0500

    added middleware and get routes

--- server.js
+++ server.js
@@ -4,12 +4,26 @@ const path = require('path');
 const fs = requidre('fs');
 const { v4: uuid } = require("uuid");
 
+// PORT
 const PORT = process.env.PORT || 3001;
+
+// Init express
 const app = express();
 
+app.use(express.static('public'));
+
+// JSON middleware
+app.use(express.json());
+
+// GET route for index
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 // GET route for notes page
-
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 // POST route to save notes
 
 // DELETE notes
