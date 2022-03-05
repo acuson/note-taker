6e4ed50ae65d2340f7f58d371bbec1eeca8eafaf
Alyssa Cuson <acuson98@gmail.com>
2022-03-05 11:36:25 -0500

    updated file structure

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Require
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index');
+const api = require('./Develop/routes/index');
 const fs = require('fs');
 
 // PORT
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use('/api', api);
 
 // GET route for index
-app.get('*', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
